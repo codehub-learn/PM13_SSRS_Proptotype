83ef5afe1ba6d1c45aef689cd3c9106b35c1bf38
Stan <sk2114@outlook.com>
2021-04-23 16:57:09 +0300

    Added Custom Code and Assembly

---
+++ CustomAssembly/CustomAssembly/MyAssembly.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAssembly
+{
+    public static class MyAssembly
+    {
+        public static string Capitalize(string text)
+        {
+
+            if (!String.IsNullOrEmpty(text))
+                return text.ToUpper();
+            else return "No Brand";
+        }
+    }
+}
